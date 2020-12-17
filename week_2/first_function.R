1da16208f0ac0bcf3cecbfa6a20dc259b12cf5fb
Puje <opta.mgl@gmail.com>
2020-12-17 22:01:11 +0800

    funtions practice

--- week_2/first_function.R
+++ week_2/first_function.R
@@ -1,2 +1,29 @@
 #First functions
 
+add2 <- function(x, y) {
+  x + y
+}
+
+above10 <- function(x) {
+  use <- x>10
+  x[use]
+}
+
+above <- function(x, n = 10) {
+  use <- x>n
+  x[use]
+}
+
+cube <- function(x, n) {
+  x^3
+}
+
+fu <- function(x) {
+  g <- function(y) { #g <y + 4
+    y + z #y + 4
+  }
+  z <- 4
+  x + g(x) #x + (x+4)
+}
+s
+
